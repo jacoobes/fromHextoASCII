f9b6242faab57f2bd30625b26f03093516942d90
jacoobes <jacobnguyend@gmail.com>
2021-07-05 13:16:36 -0500

    encode decode done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,15 @@
+import kotlin.math.pow
 
 fun main(args: Array<String>) {
     if(args.isEmpty()) throw RuntimeException("No arguments provided")
 
-    println(textToHex(args[0]))
+    when(args[0])  {
+      "--encode", "-enc" -> println( textToHex(args[1])  )
+      "--decode", "-dec" -> println( hextoText(args[1].split(" ")))
+        else -> throw RuntimeException("Not a valid flag")
+    }
+
+
 }
 fun recurseDigit(decimalCode: Int) : String {
     val quotient = decimalCode / 16
@@ -13,6 +20,24 @@ fun recurseDigit(decimalCode: Int) : String {
     else recurseDigit(quotient) + mapLetter.getOrElse(remainder) { remainder }
 }
 
-fun textToHex(codes: String): String {
-    return codes.map { recurseDigit(it.code) } .joinToString(" ")
+fun textToHex(codes: String): String = codes.map { recurseDigit(it.code) }.joinToString(" ")
+
+
+fun multiply16(hexCode : String): Char {
+
+
+    val (first, second) = hexCode
+
+    return (first + second).toChar()
 }
+private operator fun String.component1(): Int = this[0].toString().toInt() * 16
+
+private operator fun String.component2(): Int {
+    val mapLetter = mapOf('a' to 10, 'b' to 11 , 'c' to 12, 'd' to 13, 'e' to 14, 'f' to 15)
+   return if( this[1].isLetter() ) mapLetter[this[1]]!! * zero() else this[1].toString().toInt() * zero()
+}
+private fun zero() = 1.0.pow(0.0).toInt()
+
+
+
+fun hextoText(codes: List<String>) = codes.map { multiply16(it)  }.joinToString("")
